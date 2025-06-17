2b0801acccb142a1099cf4272c79883ec7d66e46
MaximilianZaurus <z.dagirov@gmail.com>
2025-06-17 20:40:10 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,24 @@
-import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
-from telegram import Update, Bot
-from telegram.ext import Application, CommandHandler, ContextTypes
-from telegram.constants import ParseMode
+from pybit.unified_trading import HTTP
+import os
+import json
 
 load_dotenv()
+bot_token = os.getenv("BOT_TOKEN")
+chat_id = os.getenv("CHAT_ID")
+client = HTTP(testnet=False)
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # подстроено под твои переменные
-CHAT_ID = os.getenv("CHAT_ID")      # обязательно укажи в .env id чата
-
-# Создаем объект Application один раз
-telegram_app = Application.builder().token(BOT_TOKEN).build()
-
-# Асинхронная функция отправки сообщений (чтобы импортировать в scheduler)
-bot = Bot(token=BOT_TOKEN)
-async def send_message(text: str):
-    await bot.send_message(chat_id=CHAT_ID, text=text, parse_mode=ParseMode.HTML)
-
-# Обработчик команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🤖 Привет! Я бот стратегии Galilei. Сигналы будут приходить автоматически.")
+    with open("tickers.json") as f:
+        tickers = json.load(f)
 
-telegram_app.add_handler(CommandHandler("start", start))
+    msg = "🟢 Бот запущен\n"
+    for ticker in tickers:
+        price = client.get_ticker(category="linear", symbol=ticker)["result"]["list"][0]["lastPrice"]
+        msg += f"{ticker}: {price}\n"
+    await context.bot.send_message(chat_id=chat_id, text=msg)
+
+app = ApplicationBuilder().token(bot_token).build()
+app.add_handler(CommandHandler("start", start))
